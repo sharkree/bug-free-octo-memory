5df415e030b9f5bac7840288c08e0cff85d65ee6
GitHub Action <action@github.com>
2023-01-15 22:44:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/sharkree/bugfreeoctomemory/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sharkree/bugfreeoctomemory/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sharkree.bugfreeoctomemory.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sharkree.bugfreeoctomemory.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/sharkree/bugfreeoctomemory/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sharkree/bugfreeoctomemory/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sharkree.bugfreeoctomemory.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sharkree.bugfreeoctomemory.MyBundle
 
 class MyProjectService(project: Project) {
 
